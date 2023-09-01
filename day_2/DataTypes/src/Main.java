25223fed772c3dd1d600c3493f61327daa742476
Himanshu <kandpallucky@gmail.com>
2023-09-01 23:05:27 +0530

    Added assignment 2 solution

---
+++ day_2/DataTypes/src/Main.java
@@ -0,0 +1,38 @@
+// program to print variables of different data types in java
+public class Main {
+    public static void main(String[] args) {
+        //boolean: unsigned 8 bits
+        boolean varBoolean = true;
+        System.out.println(varBoolean);
+
+        // byte: signed 8 bits
+        byte varByte = 2;
+        System.out.println(varByte);
+
+        // short: signed 16 bits
+        short varShort = 25000;
+        System.out.println(varShort);
+
+        // int: signed 32 bits
+        int varInt = 2000000000;
+        System.out.println(varInt);
+
+        // long: signed 64 bits
+        long varLong = 900000000000000l;
+        System.out.println(varLong);
+
+        // float: 32 bits
+        float varFloat = 3000.58f;
+        System.out.println(varFloat);
+
+        //double: 64 bits
+        double varDouble = 8500.7892227;
+        System.out.println(varDouble);
+
+        // char: unsigned 16 bits
+        char varChar = 'A';
+        System.out.println(varChar);
+
+
+    }
+}
